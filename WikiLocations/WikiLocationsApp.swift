b44786081605b66928019ff81c66942c81f86ce4
Mohamed Hasan <hasan.developer@gmail.com>
2024-05-02 23:19:04 +0200

    Initial Commit

---
+++ WikiLocations/WikiLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WikiLocationsApp.swift
+//  WikiLocations
+//
+//  Created by Mohamed Hassan on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WikiLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
