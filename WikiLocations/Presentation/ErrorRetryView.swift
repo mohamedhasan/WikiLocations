f0ff83f0be197b1475c0e2ae1986a7f93def7ef1
Mohamed Hasan <hasan.developer@gmail.com>
2024-05-03 19:17:05 +0200

    Add Error retry view

--- WikiLocations/Presentation/ErrorRetryView.swift
+++ WikiLocations/Presentation/ErrorRetryView.swift
@@ -5,4 +5,24 @@
 //  Created by Mohamed Hassan on 03/05/2024.
 //
 
-import Foundation
+import SwiftUI
+
+struct ErrorRetryView: View {
+    let error: NetworkError
+    let retryAction: () async -> Void
+
+    var body: some View {
+        VStack {
+            Text(error.errorMessage)
+                .padding()
+            Spacer()
+                .frame(height: DesignSystem.shared.bottomMargin)
+            Button(AppStrings.retryErrorButton) {
+                Task {
+                    await retryAction()
+                }
+            }
+        }
+    }
+}
+
